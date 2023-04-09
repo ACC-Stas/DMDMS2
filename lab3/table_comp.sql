5d64f9aae97cdb88c08daffa6a357a7ee32e9a48
Станислав Кононович <ksv.2002@mail.ru>
2023-04-09 18:24:29 +0300

    add PROD_DELETE_LIST

--- lab3/table_comp.sql
+++ lab3/table_comp.sql
@@ -1,12 +1,31 @@
 CREATE OR REPLACE PROCEDURE PROD_CREATE_LIST(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
-IS
+    IS
 BEGIN
-    FOR diff IN (Select  DISTINCT table_name from all_tab_columns where owner = dev_schema_name  and (table_name, column_name) not in
-        (select table_name, column_name from all_tab_columns where owner = prod_schema_name))
-    LOOP
-        DBMS_OUTPUT.PUT_LINE(diff.TABLE_NAME);
-    END LOOP;
+    FOR diff IN (Select DISTINCT table_name
+                 from all_tab_columns
+                 where owner = dev_schema_name
+                   and (table_name, column_name) not in
+                       (select table_name, column_name from all_tab_columns where owner = prod_schema_name))
+        LOOP
+            DBMS_OUTPUT.PUT_LINE(diff.TABLE_NAME);
+        END LOOP;
+END;
+
+
+CREATE OR REPLACE PROCEDURE PROD_DELETE_LIST(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
+    IS
+BEGIN
+    FOR diff IN (Select DISTINCT table_name
+                 from all_tab_columns
+                 where owner = prod_schema_name
+                   and (table_name, column_name) not in
+                       (select table_name, column_name from all_tab_columns where owner = dev_schema_name))
+        LOOP
+            DBMS_OUTPUT.PUT_LINE(diff.TABLE_NAME);
+        END LOOP;
 END;
 
 
 CALL PROD_CREATE_LIST('DEV', 'PROD');
+CALL PROD_DELETE_LIST('DEV', 'PROD');
+
