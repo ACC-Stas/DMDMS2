9770b7852a1b7691e2de3396dee93f7d9b9274df
Станислав Кононович <ksv.2002@mail.ru>
2023-04-09 21:50:40 +0300

    add order

--- lab3/create_tables.sql
+++ lab3/create_tables.sql
@@ -17,3 +17,37 @@ CREATE TABLE prod.smth1(
     CONSTRAINT smth1_pk PRIMARY KEY (id)
 );
 
+
+CREATE TABLE dev.three
+(
+    id         NUMBER       not null,
+    some_field VARCHAR2(59) not null,
+    CONSTRAINT three_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE dev.two
+(
+    id         NUMBER(10)   not null,
+    id_2 NUMBER(10) not null,
+    some_field VARCHAR2(59) not null,
+    CONSTRAINT two_pk PRIMARY KEY (id),
+    CONSTRAINT two_fk FOREIGN KEY (id_2) REFERENCES dev.three(id)
+);
+
+CREATE TABLE dev.one
+(
+    id         NUMBER(10)   not null,
+    id_2 NUMBER(10) not null,
+    some_field VARCHAR2(59) not null,
+    CONSTRAINT one_pk PRIMARY KEY (id),
+    CONSTRAINT one_fk FOREIGN KEY (id_2) REFERENCES dev.two(id)
+);
+
+CREATE TABLE fk_table
+(
+    id     NUMBER,
+    child  VARCHAR2(100),
+    parent VARCHAR2(100)
+);
+
+
